451ce68b3130a0429ca9368f561fb48ba9371187
gangjun06 <me@gangjun.dev>
2021-03-08 23:13:13 +0900

    crawl detail page

--- osang_data.go
+++ osang_data.go
@@ -30,6 +30,22 @@ type List struct {
 	CreateAt  time.Time
 }
 
+type Detail struct {
+	ID        uint
+	Title     string
+	WrittenBy string
+	CreateAt  time.Time
+	Content   string
+	Images    []string
+	Files     []File
+}
+
+type File struct {
+	Name     string
+	Download string
+	Preview  string
+}
+
 func CrawlList(url Url, page uint) ([]*List, error) {
 	res, err := http.Get(string(url) + "?page=" + strconv.Itoa(int(page)))
 	if err != nil {
@@ -61,7 +77,7 @@ func CrawlList(url Url, page uint) ([]*List, error) {
 				onclick, _ := s.Find("a").First().Attr("onclick")
 				idStr := strings.Split(onclick, "'")
 				newItem.Title = title
-				id, _ := strconv.Atoi(idStr[4])
+				id, _ := strconv.Atoi(idStr[3])
 				newItem.ID = uint(id)
 			case 2:
 				newItem.WrittenBy = s.Text()
@@ -72,5 +88,59 @@ func CrawlList(url Url, page uint) ([]*List, error) {
 		})
 		result = append(result, newItem)
 	})
+	return result, nil
+}
+
+func CrawlPage(url Url, id uint) (*Detail, error) {
+	res, err := http.Get(string(url) + "/" + strconv.Itoa(int(id)))
+	if err != nil {
+		return nil, fmt.Errorf("Error Loading")
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		return nil, fmt.Errorf("Error Loading")
+	}
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result := &Detail{
+		ID: id,
+	}
+	doc.Find(".viewBox p").Each(func(i int, s *goquery.Selection) {
+		txt := strings.ReplaceAll(s.Text(), "<br>", "")
+		result.Content += txt + "\n"
+	})
+	doc.Find(".viewBox img").Each(func(i int, s *goquery.Selection) {
+		src, _ := s.Attr("src")
+		result.Images = append(result.Images, "http://school.gy06.net"+src)
+	})
+	doc.Find(".infoBox li").Each(func(i int, s *goquery.Selection) {
+		switch i {
+		case 0:
+			result.WrittenBy = strings.ReplaceAll(s.Text(), "작성자", "")
+		case 1:
+			t, _ := time.Parse("2006-01-02", strings.ReplaceAll(s.Text(), "작성일", ""))
+			result.CreateAt = t
+		}
+	})
+	doc.Find(".fieldBox dd").Each(func(i int, s *goquery.Selection) {
+		newFile := File{}
+		s.Find("a").Each(func(i int, s *goquery.Selection) {
+			switch i {
+			case 0:
+				newFile.Name = s.Text()
+				downloadLink, _ := s.Attr("href")
+				newFile.Download = "https://school.gy06.net" + downloadLink
+			case 1:
+				previewLink, _ := s.Attr("href")
+				newFile.Preview = previewLink
+			}
+		})
+		result.Files = append(result.Files, newFile)
+	})
+
 	return result, nil
 }
--- osang_data_test.go
+++ osang_data_test.go
@@ -5,7 +5,13 @@ import "testing"
 func TestCrawl(t *testing.T) {
 	result, err := CrawlList(UrlNotice, 1)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	t.Logf("list: %+v\n", result[0])
+	detail, err := CrawlPage(UrlNotice, result[0].ID)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	t.Logf("detail: %+v\n", detail)
 }
